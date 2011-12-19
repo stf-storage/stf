cfb7ec76c1de94c1448645ec69fbad72e559f83b
lestrrat <lestrrat+github@gmail.com>
2011-12-19 15:11:16 +0900

    change license to Artistic 2.0

--- lib/STF.pm
+++ lib/STF.pm
@@ -11,27 +11,19 @@ STF - Scalable, Simple Distributed Object Storage
 
 =head1 SYNOPSIS
 
-    # More to come soon
+    see http://stf-storage.github.com
 
 =head1 DESCRIPTION
 
-STF is a distributed object storage, built with Perl, MySQL, Q4M, and Memcached.
+STF is a distributed object storage, built with Perl, MySQL, Q4M (or TheSchwartz), and Memcached.
 
 STF uses HTTP as its protocol, so it's very easy for your applications to talk to it.
-
-=head1 CARTON (EXPERIMENTAL)
-
-STF comes with experimental carton support.
-
-    git clone stf ...
-    carton install
-    carton exec STF_HOST_ID=... plackup -Ilib etc/dispatcher.psgi
 
 =head1 SEE ALSO
 
 http://stf-storage.github.com - project page.
 
-L<STF::Dispatcher>
+L<STF::Dispatcher::PSGI>
 
 L<Net::STF::Client>
 
@@ -45,10 +37,11 @@ Ikebe Tomohiro
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Daisuke Maki
+Copyright (C) 2011 by livedoor, inc.
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.0 or,
-at your option, any later version of Perl 5 you may have available.
+it under the The Artistic License 2.0 (GPL Compatible)
+
+L<http://www.opensource.org/licenses/Artistic-2.0>
 
 =cut
