7f8fb6e7c551506b9870f0774e590efaa8c926b0
Daisuke Maki <lestrrat+github@gmail.com>
2012-07-03 11:12:10 +0900

    more Log::Minimal

--- lib/STF/Trait/WithCache.pm
+++ lib/STF/Trait/WithCache.pm
@@ -1,8 +1,9 @@
 package STF::Trait::WithCache;
 use Mouse::Role;
+use STF::Constants qw(STF_CACHE_DEBUG);
+use STF::Log;
 
 with 'STF::Trait::WithContainer';
-use STF::Constants qw(STF_CACHE_DEBUG);
 
 our $DEFAULT_CACHE_EXPIRES = 5 * 60;
 has cache_expires => (
@@ -26,9 +27,7 @@ sub cache_get {
     my $key = $self->cache_key(@keys);
     my $ret = $self->get('Memcached')->get( $key );
     if (STF_CACHE_DEBUG) {
-        printf STDERR " + Cache %s for %s\n",
-            ( defined $ret ? "HIT" : "MISS" ),
-            $key
+        debugf("Cache %s for %s", ( defined $ret ? "HIT" : "MISS" ), $key);
     }
     return $ret;
 }
@@ -37,7 +36,7 @@ sub cache_set {
     my ($self, $key, $value, $expires) = @_;
     $key = ref $key eq 'ARRAY' ? $self->cache_key(@$key) : $key;
     if (STF_CACHE_DEBUG) {
-        printf STDERR " + Cache SET for %s\n", $key
+        debugf("Cache SET for %s", $key);
     }
     $self->get('Memcached')->set( $key, $value, $expires || $self->cache_expires || $DEFAULT_CACHE_EXPIRES );
 }
@@ -46,7 +45,7 @@ sub cache_delete {
     my ($self, @keys) = @_;
     my $key = $self->cache_key(@keys);
     if (STF_CACHE_DEBUG) {
-        printf STDERR " + Cache DELETE for %s\n", $key
+        debugf("Cache DELETE for %s", $key);
     }
     $self->get('Memcached')->delete( $key );
 }
