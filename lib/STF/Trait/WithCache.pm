284d29b2e0c79ac3017e5ae32e898e9adf3067c8
Daisuke Maki <lestrrat+github@gmail.com>
2012-07-25 12:40:31 +0900

    tweak cache related debug message

--- lib/STF/Trait/WithCache.pm
+++ lib/STF/Trait/WithCache.pm
@@ -13,39 +13,58 @@ has cache_expires => (
 
 sub cache_key {
     my ($self, @keys) = @_;
-    join '.', @keys;
+    my $key = join '.', @keys;
+    if (STF_CACHE_DEBUG) {
+        debugf("Generated cache key '%s' from [%s]",
+            $key,
+            join ", ", @keys
+        );
+    }
+    return $key;
 }
 
 sub cache_get_multi {
     my ($self, @keys) = @_;
+
+    local $STF::Log::PREFIX = "Cache";
     my $ret = $self->get('Memcached')->get_multi( @keys );
+    if (STF_CACHE_DEBUG) {
+        debugf("GET MULTI for (%s) returned %d values", join(", ", @keys), keys %$ret);
+        foreach my $key (@keys) {
+            debugf("   %s -> %s", $key, $ret->{$key} ? "HIT" : "MISS");
+        }
+    }
     return $ret;
 }
 
 sub cache_get {
     my ($self, @keys) = @_;
+    local $STF::Log::PREFIX = "Cache";
     my $key = $self->cache_key(@keys);
     my $ret = $self->get('Memcached')->get( $key );
     if (STF_CACHE_DEBUG) {
-        debugf("Cache %s for %s", ( defined $ret ? "HIT" : "MISS" ), $key);
+        debugf("GET %s for %s", ( defined $ret ? "HIT" : "MISS" ), $key);
     }
     return $ret;
 }
 
 sub cache_set {
     my ($self, $key, $value, $expires) = @_;
+    local $STF::Log::PREFIX = "Cache";
     $key = ref $key eq 'ARRAY' ? $self->cache_key(@$key) : $key;
     if (STF_CACHE_DEBUG) {
-        debugf("Cache SET for %s", $key);
+        debugf("SET for %s", $key);
     }
     $self->get('Memcached')->set( $key, $value, $expires || $self->cache_expires || $DEFAULT_CACHE_EXPIRES );
 }
 
 sub cache_delete {
     my ($self, @keys) = @_;
+
+    local $STF::Log::PREFIX = "Cache";
     my $key = $self->cache_key(@keys);
     if (STF_CACHE_DEBUG) {
-        debugf("Cache DELETE for %s", $key);
+        debugf("DELETE for %s", $key);
     }
     $self->get('Memcached')->delete( $key );
 }
