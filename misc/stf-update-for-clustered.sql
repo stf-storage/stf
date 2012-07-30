9403a81a01a0533c60cbda21ab2a472f6ed596a5
Daisuke Maki <lestrrat+github@gmail.com>
2012-07-30 09:59:07 +0900

    fix update sql script

--- misc/stf-update-for-clustered.sql
+++ misc/stf-update-for-clustered.sql
@@ -13,9 +13,7 @@ ALTER TABLE storage ADD FOREIGN KEY (cluster_id) REFERENCES storage_cluster (id)
 CREATE TABLE object_cluster_map (
     object_id BIGINT NOT NULL,
     cluster_id INT NOT NULL,
+    PRIMARY KEY(object_id),
     FOREIGN KEY (object_id) REFERENCES object (id) ON DELETE CASCADE,
-    FOREIGN KEY (cluster_id) REFERENCES storage_cluster (id) ON DELETE CASCADE,
-    PRIMARY KEY (object_id, cluster_id)
+    FOREIGN KEY (cluster_id) REFERENCES storage_cluster (id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
-
-
