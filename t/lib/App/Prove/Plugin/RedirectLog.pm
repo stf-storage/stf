1f29bb72b03926eb6b6b47fcfcb8011dfbedc411
Daisuke Maki <lestrrat+github@gmail.com>
2012-09-06 10:43:43 +0900

    Allow redirecting to stderr, explicitly

--- t/lib/App/Prove/Plugin/RedirectLog.pm
+++ t/lib/App/Prove/Plugin/RedirectLog.pm
@@ -2,7 +2,9 @@ package t::lib::App::Prove::Plugin::RedirectLog;
 use strict;
 
 sub load {
-    $ENV{STF_LOG_FILE} ||= "t/test.log";
+    if (!exists $ENV{STF_LOG_FILE}) {
+        $ENV{STF_LOG_FILE} = "t/test.log";
+    }
 }
 
 1;
