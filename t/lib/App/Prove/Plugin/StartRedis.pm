5d3b614910554a68f14e4bdd1c80085daa1c98c9
Daisuke Maki <lestrrat+github@gmail.com>
2012-11-27 13:56:02 +0900

    Grr, remove even more stuff

--- t/lib/App/Prove/Plugin/StartRedis.pm
+++ t/lib/App/Prove/Plugin/StartRedis.pm
@@ -42,14 +42,6 @@ auto-aof-rewrite-percentage 100
 auto-aof-rewrite-min-size 64mb
 slowlog-log-slower-than 10000
 slowlog-max-len 128
-hash-max-zipmap-entries 512
-hash-max-zipmap-value 64
-list-max-ziplist-entries 512
-list-max-ziplist-value 64
-set-max-intset-entries 512
-zset-max-ziplist-entries 128
-zset-max-ziplist-value 64
-activerehashing yes
 EOM
 
         diag "Starting memcached on 127.0.0.1:$port";
