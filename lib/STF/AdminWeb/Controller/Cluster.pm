06dc614fbe7d0b16b34eb868d58ea0796ff67b34
Daisuke Maki <lestrrat+github@gmail.com>
2013-02-19 15:29:45 +0900

    migrate cluster stuff

--- lib/STF/AdminWeb/Controller/Cluster.pm
+++ lib/STF/AdminWeb/Controller/Cluster.pm
@@ -1,52 +1,51 @@
 package STF::AdminWeb::Controller::Cluster;
-use Mouse;
+use Mojo::Base 'STF::AdminWeb::Controller';
 use JSON ();
-extends 'STF::AdminWeb::Controller';
 
 sub load_cluster {
-    my ($self, $c) = @_;
-    my $cluster_id = $c->match->{object_id};
-    my $cluster = $c->get('API::StorageCluster')->lookup( $cluster_id );
+    my ($self) = @_;
+    my $cluster_id = $self->match->captures->{object_id};
+    my $cluster = $self->get('API::StorageCluster')->lookup( $cluster_id );
     if (! $cluster) {
-        $c->res->status(404);
-        $c->abort;
+        return;
     }
-    $c->stash->{cluster} = $cluster;
+    $self->stash(cluster => $cluster);
     return $cluster;
 }
 
 sub view {
-    my ($self, $c) = @_;
-    if (! $self->load_cluster($c) ) {
+    my ($self) = @_;
+    if (! $self->load_cluster() ) {
+        $self->render_not_found();
         return;
     }
-    my $cluster = $c->stash->{cluster};
-    my $storage_api = $c->get('API::Storage');
+    my $cluster = $self->stash(cluster};
+    my $storage_api = $self->get('API::Storage');
     my @storages = $storage_api->search({ cluster_id => $cluster->{id} });
     $cluster->{storages} = \@storages;
 }
 
 sub add {}
 sub add_post {
-    my ($self, $c) = @_;
+    my ($self) = @_;
 
-    my $params = $c->request->parameters->as_hashref;
-    my $result = $self->validate( $c, cluster_add => $params );
+    my $params = $self->req->params->to_hash;
+    my $result = $self->validate( cluster_add => $params );
     if ($result->success) {
         my $valids = $result->valid;
-        $c->get('API::StorageCluster')->create( $valids );
-        $c->redirect( $c->uri_for('/cluster', {done => 1}) );
+        $self->get('API::StorageCluster')->create( $valids );
+        $self->redirect_to( $self->url_for('/cluster', {done => 1}) );
     } else {
-        $c->stash->{template} = 'cluster/add';
+        $self->stash(template => 'cluster/add');
     }
 }
 
 sub list {
-    my ($self, $c) = @_;
+    my ($self) = @_;
 
-    my $limit = $c->request->param('limit') || 100;
-    my $pager = $c->pager( $limit );
-    my @clusters = $c->get( 'API::StorageCluster' )->search(
+    my $limit = $self->req->param('limit') || 100;
+    my $pager = $self->pager( $limit );
+    my @clusters = $self->get( 'API::StorageCluster' )->search(
         {},
         {
             limit    => $pager->entries_per_page + 1,
@@ -59,86 +58,91 @@ sub list {
         pop @clusters;
     }
 
-    my $storage_api = $c->get('API::Storage');
+    my $storage_api = $self->get('API::Storage');
     foreach my $cluster ( @clusters ) {
         my @storages = $storage_api->search({ cluster_id => $cluster->{id} });
         $cluster->{storages} = \@storages;
     }
 
-    my $stash = $c->stash;
+    my $stash = $self->stash;
     $stash->{clusters} = \@clusters;
     $stash->{pager} = $pager;
 }
 
 sub storage_change {
-    my ($self, $c) = @_;
+    my ($self) = @_;
 
-    my $cluster_id = $c->request->param('cluster_id');
-    my $storage_api = $c->get('API::Storage');
-    foreach my $storage_id ( $c->request->parameters->get_all('storage_id[]') ) {
+    my $cluster_id = $self->req->param('cluster_id');
+    my $storage_api = $self->get('API::Storage');
+    foreach my $storage_id ( $self->req->parameters->get_all('storage_id[]') ) {
         $storage_api->update( $storage_id, {
             cluster_id => $cluster_id
         } );
     }
 
-    my $response = $c->response;
-    $response->code(200);
-    $response->content_type('application/json');
-    $response->body( JSON::encode_json( {} ) );
-
-    $c->finished(1);
+    $self->render_json({});
 }
 
 sub storage_unclustered {
-    my ($self, $c) = @_;
-    my @storages = $c->get('API::Storage')->search({ cluster_id => undef });
-    $c->stash->{cluster_id} = $c->request->param('id');
-    $c->stash->{storages} = \@storages;
+    my ($self) = @_;
+    my @storages = $self->get('API::Storage')->search({ cluster_id => undef });
+    $self->stash(cluster_id => $self->req->param('id'));
+    $self->stash(storages => \@storages);
 }
 
 sub edit {
-    my ($self, $c) = @_;
+    my ($self) = @_;
 
-    my $cluster = $self->load_cluster($c);
-    $self->fillinform( $c, $cluster );
+    my $cluster = $self->load_cluster();
+    if (! $cluster) {
+        $self->render_not_found;
+        return;
+    }
+    $self->fillinform( $cluster );
 }
 
 sub edit_post {
-    my ($self, $c) = @_;
-    my $cluster = $self->load_cluster($c);
+    my ($self) = @_;
+    my $cluster = $self->load_cluster();
+    if (! $cluster) {
+        $self->render_not_found;
+        return;
+    }
 
-    my $params = $c->request->parameters->as_hashref;
+    my $params = $self->req->params->to_hash;
     $params->{id} = $cluster->{id};
-    my $result = $self->validate( $c, cluster_edit => $params );
+    my $result = $self->validate( cluster_edit => $params );
     if ($result->success) {
         my $valids = $result->valid;
         my %meta;
         delete $valids->{id};
-        my $api = $c->get('API::StorageCluster');
+        my $api = $self->get('API::StorageCluster');
         $api->update( $cluster->{id} => $valids );
 
-        $c->redirect( $c->uri_for( "/cluster/list", { done => 1 } ) );
+        $self->redirect_to( $self->url_for( "/cluster/list", { done => 1 } ) );
     } else {
-        $c->stash->{template} = 'cluster/edit';
-        $self->fillinform( $c, $params );
+        $self->stash(template => 'cluster/edit');
+        $self->fillinform( $params );
     }
 }
 
 sub delete_post {
-    my ($self, $c) = @_;
+    my ($self) = @_;
 
-    my $cluster = $self->load_cluster($c);
+    my $cluster = $self->load_cluster();
+    if (! $cluster) {
+        $self->render_not_found;
+        return;
+    }
     my $params = { id => $cluster->{id} };
-    my $result = $self->validate( $c, cluster_delete => $params );
+    my $result = $self->validate( cluster_delete => $params );
     if ( $result->success ) {
-        $c->get('API::StorageCluster')->delete( $cluster->{id} );
-        $c->redirect( $c->uri_for( '/cluster/list', {done => 1}) );
+        $self->get('API::StorageCluster')->delete( $cluster->{id} );
+        $self->redirect_to( $self->url_for( '/cluster/list', {done => 1}) );
     } else {
-        $c->stash->{template} = 'cluster/edit';
-        $self->fillinform( $c, $params );
+        $self->stash(template => 'cluster/edit');
+        $self->fillinform( $params );
     }
 }
-
-no Mouse;
 
 1;
